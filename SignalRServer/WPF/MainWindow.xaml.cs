6876e0e621aaedd81cadffe36c66e6b8bbf62d42
Rugile Karengaite <rugile.karengaite@ktu.edu>
2022-09-21 21:16:34 +0300

    add walking pacman that can collect points

--- SignalRServer/WPF/MainWindow.xaml.cs
+++ SignalRServer/WPF/MainWindow.xaml.cs
@@ -3,6 +3,12 @@ using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Linq;
 
 namespace WPF
 {
@@ -12,6 +18,19 @@ namespace WPF
     public partial class MainWindow : Window
     {
         private HubConnection _connection;
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        bool goLeft, goRight, goUp, goDown;
+        bool noLeft, noRight, noUp, noDown;
+
+        int speed = 8;
+
+        Rect pacmanHitBox;
+
+        int ghostSpeed = 10;
+        int ghostMoveStep = 130;
+        int currentGhostStep;
+        int score = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -23,7 +42,8 @@ namespace WPF
                 await Task.Delay(new Random().Next(0, 5) * 1000);
                 await _connection.StartAsync();
             };
-              
+
+            GameSetup();
         }
 
         private async void BtnConnect_Click(object sender, RoutedEventArgs e)
@@ -75,6 +95,184 @@ namespace WPF
         private void AddToList(string value)
         {
             messagesList.Items.Add(value);
+        }
+
+        private void CanvasKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+            if (e.Key == Key.Left && !noLeft)
+            {
+                goRight = goUp = goDown = false;
+                noRight = noUp = noDown = false; 
+
+                goLeft = true;
+
+                pacman.RenderTransform = new RotateTransform(-180, pacman.Width / 2, pacman.Height / 2);
+            }
+
+            if (e.Key == Key.Right && !noRight)
+            {
+                noLeft = noUp = noDown = false;
+                goLeft = goUp = goDown = false; 
+
+                goRight = true; 
+
+                pacman.RenderTransform = new RotateTransform(0, pacman.Width / 2, pacman.Height / 2); 
+
+            }
+
+            if (e.Key == Key.Up && !noUp)
+            {
+                noRight = noDown = noLeft = false; 
+                goRight = goDown = goLeft = false; 
+
+                goUp = true; 
+
+                pacman.RenderTransform = new RotateTransform(-90, pacman.Width / 2, pacman.Height / 2);
+            }
+
+            if (e.Key == Key.Down && !noDown)
+            {
+                noUp = noLeft = noRight = false; 
+                goUp = goLeft = goRight = false; 
+
+                goDown = true; 
+
+                pacman.RenderTransform = new RotateTransform(90, pacman.Width / 2, pacman.Height / 2);
+            }
+        }
+
+        private void GameSetup()
+        {
+            MyCanvas.Focus();
+            gameTimer.Tick += GameLoop;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20); ///will tick every 20ms
+            gameTimer.Start();
+            currentGhostStep = ghostMoveStep;
+
+            //add images for pacman
+            ImageBrush pacmanBrush = new ImageBrush();
+            pacmanBrush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/pacman.jpg"));
+            pacman.Fill = pacmanBrush;
+
+            //add images to ghosts too 
+            ImageBrush redGhost = new ImageBrush();
+            redGhost.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/red.jpg"));
+            redguy.Fill = redGhost;
+            ImageBrush orangeGhost = new ImageBrush();
+            orangeGhost.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/orange.jpg"));
+            orangeguy.Fill = orangeGhost;
+            ImageBrush pinkGhost = new ImageBrush();
+            pinkGhost.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/pink.jpg"));
+            pinkguy.Fill = pinkGhost;
+        }
+
+        private void GameLoop(object? sender, EventArgs e)
+        {
+
+            txtScore.Content = "Score: " + score; // show the scoreo to the txtscore label. 
+
+            if (goRight)
+            {
+                Canvas.SetLeft(pacman, Canvas.GetLeft(pacman) + speed);
+            }
+            if (goLeft)
+            {
+                Canvas.SetLeft(pacman, Canvas.GetLeft(pacman) - speed);
+            }
+            if (goUp)
+            {
+                Canvas.SetTop(pacman, Canvas.GetTop(pacman) - speed);
+            }
+            if (goDown)
+            {
+                Canvas.SetTop(pacman, Canvas.GetTop(pacman) + speed);
+            }
+
+            if (goDown && Canvas.GetTop(pacman) + 280 > Application.Current.MainWindow.Height)
+            {
+                noDown = true;
+                goDown = false;
+            }
+            if (goUp && Canvas.GetTop(pacman) < 5)
+            {
+                noUp = true;
+                goUp = false;
+            }
+            if (goLeft && Canvas.GetLeft(pacman) - 10 < 1)
+            {
+                noLeft = true;
+                goLeft = false;
+            }
+            if (goRight && Canvas.GetLeft(pacman) + 70 > Application.Current.MainWindow.Width)
+            {
+                noRight = true;
+                goRight = false;
+            }
+
+            pacmanHitBox = new Rect(Canvas.GetLeft(pacman), Canvas.GetTop(pacman), pacman.Width, pacman.Height);
+
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                // loop through all of the rectangles inside of the game and identify them using the x variable
+
+                Rect hitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height); // create a new rect called hit box for all of the available rectangles inside of the game
+
+                // find the walls, if any of the rectangles inside of the game has the tag wall inside of it
+                if ((string)x.Tag == "wall")
+                {
+                    // check if we are colliding with the wall while moving left if true then stop the pac man movement
+                    if (goLeft == true && pacmanHitBox.IntersectsWith(hitBox))
+                    {
+                        Canvas.SetLeft(pacman, Canvas.GetLeft(pacman) + 10);
+                        noLeft = true;
+                        goLeft = false;
+                    }
+                    // check if we are colliding with the wall while moving right if true then stop the pac man movement
+                    if (goRight == true && pacmanHitBox.IntersectsWith(hitBox))
+                    {
+                        Canvas.SetLeft(pacman, Canvas.GetLeft(pacman) - 10);
+                        noRight = true;
+                        goRight = false;
+                    }
+                    // check if we are colliding with the wall while moving down if true then stop the pac man movement
+                    if (goDown == true && pacmanHitBox.IntersectsWith(hitBox))
+                    {
+                        Canvas.SetTop(pacman, Canvas.GetTop(pacman) - 10);
+                        noDown = true;
+                        goDown = false;
+                    }
+                    // check if we are colliding with the wall while moving up if true then stop the pac man movement
+                    if (goUp == true && pacmanHitBox.IntersectsWith(hitBox))
+                    {
+                        Canvas.SetTop(pacman, Canvas.GetTop(pacman) + 10);
+                        noUp = true;
+                        goUp = false;
+                    }
+                }
+
+                // check if the any of the rectangles has a coin tag inside of them
+                if ((string)x.Tag == "coin")
+                {
+                    // if pac man collides with any of the coin and coin is still visible to the screen
+                    if (pacmanHitBox.IntersectsWith(hitBox) && x.Visibility == Visibility.Visible)
+                    {
+                        // set the coin visiblity to hidden
+                        x.Visibility = Visibility.Hidden;
+                        // add 1 to the score
+                        score++;
+                    }
+                }
+            }
+        }
+
+        private void GameOver(string message)
+        {
+            gameTimer.Stop();
+            MessageBox.Show(message, "PacMan Multiplayer");
+            
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location); //start a new game
+            Application.Current.Shutdown(); //shtdown current game 
         }
     }
 }
