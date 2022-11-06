580f117fb7006224159bffe58b74d1f945d4f44a
siciovas <rokas.siciovas@inbox.lt>
2022-11-06 22:38:04 +0200

    refactor adapter

--- SignalRServer/WPF/Game/Utils/Utils.cs
+++ SignalRServer/WPF/Game/Utils/Utils.cs
@@ -8,50 +8,6 @@ namespace WPF.Game.Utils
 {
     public class Utils
     {
-        public static ObservableCollection<Coin> GetCoins(CoinFactory _coinFactory)
-        {
-            ObservableCollection<Coin> result = new ObservableCollection<Coin>();
-            for (int i = 10; i < 800; i = i + 50)
-            {
-                for (int j = 50; j < 600; j = j + 50)
-                {
-                    if (i == 10 && j == 50) continue;
-                    var coin = _coinFactory.GetCoin(i, j);
-                    result.Add(coin);
-                }
-            }
-            return result;
-        }
-
-        public static ObservableCollection<Coin> GetFirstHalfCoins(CoinFactory _coinFactory)
-        {
-            ObservableCollection<Coin> result = new ObservableCollection<Coin>();
-            for (int i = 10; i < 800; i = i + 50)
-            {
-                for (int j = 50; j < 300; j = j + 50)
-                {
-                    if (i == 10 && j == 50) continue;
-                    var coin = _coinFactory.GetCoin(i, j);
-                    result.Add(coin);
-                }
-            }
-            return result;
-        }
-
-        public static ObservableCollection<Coin> GetSecondHalfCoins(CoinFactory _coinFactory, ObservableCollection<Coin> Coins)
-        {
-            //ObservableCollection<Coin> result = new ObservableCollection<Coin>();
-            for (int i = 10; i < 800; i = i + 50)
-            {
-                for (int j = 300; j < 600; j = j + 50)
-                {
-                    var coin = _coinFactory.GetCoin(i, j);
-                    Coins.Add(coin);
-                }
-            }
-            return Coins;
-        }
-
         public static ObservableCollection<Apple> CreateApples()
         {
             ObservableCollection<Apple> result = new ObservableCollection<Apple>();
